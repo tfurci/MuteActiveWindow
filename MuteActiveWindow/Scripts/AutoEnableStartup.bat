56ac62fb69d9cbaca8bff5048f331cec75fb8fbf
tfurci <48729601+tfurci@users.noreply.github.com>
2023-12-16 12:27:49 +0100

    Admin prompt for AutoEnableStartup.bat
    Add UAC prompt to fix creating .lnk in startup folder in some cases

--- MuteActiveWindow/Scripts/AutoEnableStartup.bat
+++ MuteActiveWindow/Scripts/AutoEnableStartup.bat
@@ -1,6 +1,38 @@
 @echo off
+if "%~1"=="admin" goto gotAdmin
 
-:: Define the source and destination paths
+:: Check for administrative permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Administrative privileges are required to run this script.
+    :prompt
+    choice /C YN /M "Do you want to run this script as an administrator? [Y/N]: "
+    if errorlevel 2 (
+        echo Script execution cancelled.
+        pause
+        exit /B
+    )
+    if errorlevel 1 (
+        goto UACPrompt
+    )
+) else ( 
+    goto gotAdmin 
+)
+
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~0", "admin", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
 set "ScriptDir=%~dp0..\"    :: Navigate up one directory
 set "ScriptName=MuteActiveWindow.ahk"
 set "ShortcutName=MuteActiveWindow.lnk"
@@ -14,13 +46,13 @@ if not exist "%ScriptDir%%ScriptName%" (
 )
 
 :: Check if the shortcut already exists in the startup folder
-if exist "%StartupFolder%\%ShortcutName%" (
-    del "%StartupFolder%\%ShortcutName%"
-    echo The existing shortcut "%ShortcutName%" has been deleted so new one can be created.
+if exist "%StartupFolder%%ShortcutName%" (
+    del "%StartupFolder%%ShortcutName%"
+    echo The existing shortcut "%ShortcutName%" has been deleted so a new one can be created.
 )
 
 :: Create a symbolic link (shortcut) to the script in the startup folder
-mklink "%StartupFolder%\%ShortcutName%" "%ScriptDir%%ScriptName%"
+mklink "%StartupFolder%%ShortcutName%" "%ScriptDir%%ScriptName%"
 
 echo The shortcut "%ShortcutName%" has been successfully created in the startup folder.
 pause
