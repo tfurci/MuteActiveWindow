2e0560233bcfebc8bcf0fbe5b1316216fd85dfbe
tfurci <48729601+tfurci@users.noreply.github.com>
2023-12-17 13:14:26 +0100

    Update UpdateMAW.bat

--- MuteActiveWindow/Scripts/UpdateMAW.bat
+++ MuteActiveWindow/Scripts/UpdateMAW.bat
@@ -7,9 +7,10 @@ set "rootDir=%currentDir%.."
 :: Define a list of unnecessary files to be deleted
 set "filesToDelete=Scripts\BatUpdater.bat"
 
+echo.
 :: Loop through each file in the list
 for %%f in (%filesToDelete%) do (
-    echo Removing unnecessary files.
+    echo Removing unnecessary files...
     if exist "%rootDir%\%%f" (
         del "%rootDir%\%%f"
         echo Deleted %%f
@@ -17,6 +18,7 @@ for %%f in (%filesToDelete%) do (
         echo %%f not found.
     )
 )
+echo.
 
 
 set "betaFlag=%~1"
@@ -26,17 +28,11 @@ if /i "%betaFlag%"=="-beta" (
     set "githubMainScriptURL=https://raw.githubusercontent.com/tfurci/MuteActiveWindow/main/MuteActiveWindow/MuteActiveWindow.ahk"
 )
 
-:: Specify the root directory where the script is currently located
-set "scriptDirectory=%~dp0"
-
-:: Specify the "Scripts" directory relative to the script directory
-set "scriptsDirectory=%scriptDirectory%.."
-
 :: Specify the full path to the local main script file
-set "localMainScriptPath=%scriptsDirectory%\MuteActiveWindow.ahk"
+set "localMainScriptPath=%rootDir%\MuteActiveWindow.ahk"
 
 :: Ensure that the local "Scripts" directory exists
-md "%scriptsDirectory%" 2>nul
+md "%rootDir%" 2>nul
 
 :: Download and update the main script from GitHub
 curl -k -o "%localMainScriptPath%.temp" "%githubMainScriptURL%"
@@ -57,20 +53,19 @@ if errorlevel 1 (
 )
 
 :: Specify the full path to the maw-muter.exe file
-set "localMawMuterPath=%scriptsDirectory%\maw-muter.exe"
+set "localMawMuterPath=%rootDir%\maw-muter.exe"
 
 :: Specify the URL for the latest version of maw-muter.exe on GitHub
 set "githubMawMuterURL=https://github.com/tfurci/maw-muter/releases/latest/download/maw-muter.exe"
 
 :: Check if maw-muter.exe exists in the scripts directory
 if exist "%localMawMuterPath%" (
-    echo.
     echo Updating maw-muter.exe...
     curl -L -o "%localMawMuterPath%.temp" "%githubMawMuterURL%"
     
     :: Compare the content of the downloaded maw-muter.exe file with the local file
     fc "%localMawMuterPath%.temp" "%localMawMuterPath%" > nul
-
+	echo.
     if errorlevel 1 (
         move /y "%localMawMuterPath%.temp" "%localMawMuterPath%" > nul
         echo maw-muter.exe updated successfully.
@@ -81,23 +76,17 @@ if exist "%localMawMuterPath%" (
 ) else (
     echo maw-muter.exe does not exist in the scripts directory.
 )
-
-:: Set the path to the previous folder
-set "previousFolder=..\"
-
-:: Set the name of the AutoHotkey script
-set "scriptName=MuteActiveWindow.ahk"
+echo.
 
 :: Combine the path and script name to create the full path to the script
-set "scriptPath=%previousFolder%%scriptName%"
+set "scriptPath=%rootDir%/MuteActiveWindow.ahk"
 
 :: Check if the script file exists
 if exist "%scriptPath%" (
-    echo Running %scriptName%...
+    echo Running MuteActiveWindow...
     start "" /b "%scriptPath%"
 ) else (
     echo The script %scriptName% does not exist in the previous folder. Or is not named MuteActiveWindow.ahk
-    pause
 )
 
 pause
